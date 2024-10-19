97510e2493c92444cb388b987aafd3e30911bc82
Jane051 <lolo.l@seznam.cz>
2024-10-19 11:30:58 +0200

    update navbar, tlacitka, barva

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'viewer/static/media')]
 
 
 # Default primary key field type
@@ -130,4 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'viewer/static/media')
